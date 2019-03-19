4e42f8180f959ef2f6afe2cf52b25a76e3e51cca
Richard Soeteman <richard@soetemansoftware.nl>
2019-03-19 09:02:57 +0100

    V8 Check-in

--- PackageMigrationTester/Package/6c0f5c03-39a2-44d4-8fd3-711cfcc4668d/packagemigrationtester.resources.js
+++ PackageMigrationTester/Package/6c0f5c03-39a2-44d4-8fd3-711cfcc4668d/packagemigrationtester.resources.js
@@ -7,11 +7,8 @@
             getMigrations: function (packageName) {
                 return $http.get("backoffice/packagemigrationtester/packagemigrationtesterapi/getmigrations", { params: { packageName: packageName } });
             },
-            up: function (migration) {
-                return $http.post("backoffice/packagemigrationtester/packagemigrationtesterapi/up", migration);
-            },
-            down: function (migration) {
-                return $http.post("backoffice/packagemigrationtester/packagemigrationtesterapi/down", migration);
+            migrate: function (migration) {
+                return $http.post("backoffice/packagemigrationtester/packagemigrationtesterapi/migrate", migration);
             }
         };
     });
